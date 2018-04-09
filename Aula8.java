cf63cf66e7c4e0e2d9c31a4657969d2edb38f8f5
lucasjukr94 <lucasjukr94@gmail.com>
2018-04-09 20:23:03 -0300

    aula 8

---
+++ Aula8.java
@@ -0,0 +1,20 @@
+/*
+Modelagem de Sistemas
+-Diagrama UML
+-Diagrama de Classe
+-Diagrama de Sequência
+-Diagrama de Estados
+
+Exercício: Fazer o diagrama de classes para o exercício Conta Corrente
+Exercício: Modificar o programa Conta Corrente
+
+Fazer a lista de exercícios
+*/
+
+import java.util.Scanner;
+
+public class Aula8{
+   public static void main(String args[]){
+      
+   }
+}
